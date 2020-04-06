7ae69657f589ce24a0fe19ab2b2bb60788c1ef95
zhaoyu <460472353@qq.com>
2020-04-06 15:27:53 +0800

    day3_CMS页面管理开发

--- xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
@@ -1,7 +1,10 @@
 package com.xuecheng.api.cms;
 
+import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.domain.cms.response.CmsPageResult;
 import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -15,4 +18,18 @@ public interface CmsPageControllerApi {
             @ApiImplicitParam(name="size",value = "每页记录数",required=true,paramType="path",dataType="int")
         })
     public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) ;
+
+    //根据页面id查询页面信息
+    @ApiOperation("根据页面id查询页面信息")
+    public CmsPage findById(String id);
+    //增加页面
+    @ApiOperation("增加页面")
+    public CmsPageResult add(CmsPage cmsPage);
+    //修改页面
+    @ApiOperation("修改页面")
+    public CmsPageResult edit(String id, CmsPage cmsPage);
+
+    //删除页面
+    @ApiOperation("删除页面")
+    public ResponseResult delete(String id);
 }
